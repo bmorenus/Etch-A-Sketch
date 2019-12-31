412efb38d753360bae28e5b56de5ccadbd0a3e37
Brian <brian.morenus@gmail.com>
2019-12-30 23:27:48 -0500

    implemented drawSketch function and added eventListener to each grid div

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 class Sketch {
 
-    constructor(xBoxes = 300, yBoxes = 300) {
+    constructor(numBoxes = 100) {
         this.width = document.getElementById('sketch-box').getBoundingClientRect().width;
         this.height = document.getElementById('sketch-box').getBoundingClientRect().height;
-        this.xBoxes = xBoxes;
-        this.yBoxes = yBoxes;
-        this.boxColor = "red";
+        this.sketchBox = document.getElementById("sketch-box");
+        this.numBoxes = numBoxes;
     }
 
-    
-    drawSketch(xBoxes = this.xBoxes, yBoxes = this.yBoxes, colorValue = this.boxColor) {
-        const sketchBox = document.getElementById("sketch-box");
-        const boxSize = (this.width / this.xBoxes);
-        for (let i = 0; i < xBoxes; i++) {
+    loadSketch() {
+        const boxSize = (this.width / this.numBoxes) - 2;
+        console.log(boxSize);
+        for (let i = 0; i < this.numBoxes; i++) {
             let divContainer = document.createElement("div");
-            sketchBox.appendChild(divContainer);
-            for (let j = 0; j < yBoxes; j++) {
+            this.sketchBox.appendChild(divContainer);
+            for (let j = 0; j < this.numBoxes; j++) {
                 let box = document.createElement("div");
-                box.style.height = `${boxSize}px`
-                box.style.width = `${boxSize}px`
+                box.style.height = `${boxSize}px`;
+                box.style.width = `${boxSize}px`;
                 divContainer.appendChild(box);
+                box.addEventListener('mouseover', this.drawSketch, false);
             }   
         }
     }
-    draw
+    
+    drawSketch(event) { 
+        event.target.style.background = "red";
+    }
 }
 
-function run() {
+function main() {
     sketch = new Sketch();
-    sketch.drawSketch();
+    sketch.loadSketch();
 }
 
-run();
+main();
