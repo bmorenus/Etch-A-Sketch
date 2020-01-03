22e45c299d4cec14d7c2403ccae382a88cad4140
Brian <brian.morenus@gmail.com>
2020-01-02 21:57:25 -0500

    removed unused update function

--- index.js
+++ index.js
@@ -66,9 +66,4 @@ function main() {
     sketch.loadSketch();
 }
 
-function update(jscolor) {
-    // 'jscolor' instance can be used as a string
-    document.getElementById('rect').style.backgroundColor = '#' + jscolor;
-}
-
 main();
